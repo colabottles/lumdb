e093a40505c180fd0ac61fcd1e0f3a8acf24fa16
Todd Libby <toddlibby@protonmail.com>
2020-12-16 10:13:20 -0500

    Updated registerServiceWorker.js

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -1,108 +1,109 @@
-// In production, we register a service worker to serve assets from local cache.
+/* eslint-disable */
 
-// This lets the app load faster on subsequent visits in production, and gives
-// it offline capabilities. However, it also means that developers (and users)
-// will only see deployed updates on the "N+1" visit to a page, since previously
-// cached resources are updated in the background.
+const MAX_WAIT = 500;
+const CACHEVERSION = 'v2';
+const CACHEDSTATICASSETS = [
+  '/',
+  '/index.html',
+  '/favicon.ico',
+  '/blog/index.html',
+  '/css/style.css'
+];
+const OFFLINE_PAGE = '/offline.html';
 
-// To learn more about the benefits of this model, read https://goo.gl/KwvDNy.
-// This link also includes instructions on opting out of this behavior.
+const THIS_SITE = `${self.location.protocol}//${self.location.host}`;
 
-const isLocalhost = Boolean(
-  window.location.hostname === 'localhost'
-    // [::1] is the IPv6 localhost address.
-    || window.location.hostname === '[::1]'
-    // 127.0.0.1/8 is considered localhost for IPv4.
-    || window.location.hostname.match(
-      /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/,
-    ),
-);
-
-export default function register() {
-  if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
-    // The URL constructor is available in all browsers that support SW.
-    const publicUrl = new URL(process.env.PUBLIC_URL, window.location);
-    if (publicUrl.origin !== window.location.origin) {
-      // Our service worker won't work if PUBLIC_URL is on a different origin
-      // from what our page is served on. This might happen if a CDN is used to
-      // serve assets; see https://github.com/facebookincubator/create-react-app/issues/2374
-      return;
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then(function (registrations) {
+    if (registrations.length) { // returns installed service workers
+      for(let registration of registrations) {
+        registration.unregister();
+      }
     }
-
-    window.addEventListener('load', () => {
-      const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
-
-      if (!isLocalhost) {
-        // Is not local host. Just register service worker
-        registerValidSW(swUrl);
-      } else {
-        // This is running on localhost. Lets check if a service worker still exists or not.
-        checkValidServiceWorker(swUrl);
-      }
-    });
-  }
+  });
 }
+self.addEventListener('install', function(event) {
+  caches.open(CACHEVERSION).then(function(cache) {
+    cache.addAll(CACHEDSTATICASSETS)
+    cache.add(OFFLINE_PAGE);
+  }).then(self.skipWaiting());
+});
 
-function registerValidSW(swUrl) {
-  navigator.serviceWorker
-    .register(swUrl)
-    .then((registration) => {
-      registration.onupdatefound = () => {
-        const installingWorker = registration.installing;
-        installingWorker.onstatechange = () => {
-          if (installingWorker.state === 'installed') {
-            if (navigator.serviceWorker.controller) {
-              // At this point, the old content will have been purged and
-              // the fresh content will have been added to the cache.
-              // It's the perfect time to display a "New content is
-              // available; please refresh." message in your web app.
-              console.log('New content is available; please refresh.');
-            } else {
-              // At this point, everything has been precached.
-              // It's the perfect time to display a
-              // "Content is cached for offline use." message.
-              console.log('Content is cached for offline use.');
-            }
-          }
-        };
-      };
+self.addEventListener('activate', function(event) {
+  const currentCaches = [CACHEDSTATICASSETS];
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return cacheNames.filter(function(cacheName) {
+        !currentCaches.includes(cacheName)
+      });
+    }).then(function(cachesToDelete) {
+      return Promise.all(cachesToDelete.map(
+        function(cacheToDelete) {
+          return caches.delete(cacheToDelete);
+        }
+      ));
+    }).then(function() {
+      self.clients.claim()
     })
-    .catch((error) => {
-      console.error('Error during service worker registration:', error);
-    });
-}
+  );
+});
 
-function checkValidServiceWorker(swUrl) {
-  // Check if the service worker can be found. If it can't reload the page.
-  fetch(swUrl)
-    .then((response) => {
-      // Ensure service worker exists, and that we really are getting a JS file.
-      if (
-        response.status === 404
-        || response.headers.get('content-type').indexOf('javascript') === -1
-      ) {
-        // No service worker found. Probably a different app. Reload the page.
-        navigator.serviceWorker.ready.then((registration) => {
-          registration.unregister().then(() => {
-            window.location.reload();
-          });
+self.addEventListener('fetch', function(event) {
+  if(
+    (event.request.url.startsWith(THIS_SITE)) &&
+    (event.request.method == "GET")
+  ) { // Not X-domain
+    // Try network and if it fails, go for the cached copy.
+    event.respondWith(
+      fromNetwork(event.request, MAX_WAIT)
+        .catch(function () {
+          return fromCache(event.request);
+        })
+    );
+  }
+});
+
+// Time limited network request. If the network fails or the response is not
+// served before timeout, the promise is rejected.
+function fromNetwork(request, timeout) {
+  return new Promise(function (fulfill, reject) {
+    // Reject in case of timeout.
+    var timeoutId = setTimeout(reject, timeout);
+    fetch(request).then(function (response) {
+      clearTimeout(timeoutId);
+
+      if (response.ok) {
+        // update stored in cache
+        caches.open(CACHEVERSION).then(function(cache) {
+          cache.put(request, response);
         });
-      } else {
-        // Service worker found. Proceed as normal.
-        registerValidSW(swUrl);
+
+        fulfill(response.clone()); // Fulfill in case of success.
       }
-    })
-    .catch(() => {
-      console.log(
-        'No internet connection found. App is running in offline mode.',
-      );
-    });
+      if(response.status) {
+        // Respect 4XX errors and remove from cache
+        if(`${response.status}`[0] === '4') { caches.open(CACHEVERSION).then(function (cache) { cache.delete(request); }) }
+        // Resiliency against 5XX errors
+        if(`${response.status}`[0] === '5') { reject(); }
+        fulfill(response.clone()); // Fulfill using server response
+      }
+    }).catch(reject); // Reject also if network fetch rejects.
+  });
 }
 
-export function unregister() {
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.ready.then((registration) => {
-      registration.unregister();
+// Open the cache where the assets were stored and search for the requested
+// resource. In case of no matching, if 'text/html' is requested (headers),
+// then the the offline fallback page is rendered. Otherwise the promise
+// still resolves, but it does with `undefined` as value, which shows as a
+// Network error.
+function fromCache(request) {
+  return caches.open(CACHEVERSION).then(function (cache) {
+    return cache.match(request).then(function (matching) {
+      return matching || Promise.reject('no-match');
+    }).catch(function(err) {
+      if (request.headers.get('Accept').includes('text/html')) {
+        return caches.match(OFFLINE_PAGE);
+      }
     });
-  }
+  });
 }
